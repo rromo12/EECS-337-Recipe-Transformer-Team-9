9b0cac19b6868da36c568a25b935d4b4d2097819
Matthew Heston <matthewheston@gmail.com>
2016-03-10 17:30:02 -0600

    First go at fish remover.

--- Team9/pescatarian_transformer.py
+++ Team9/pescatarian_transformer.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import nltk
+import random
 
 #given a recipe dictionary of lists of dictionaries / lists -> transform to new object of same format
 
@@ -61,6 +62,23 @@ def load_lists():
 def remove_meat(recipe):
 	for single_ingredient in recipe['ingredients']:
 		print single_ingredient['name']
+
+def remove_fish(recipe):
+    fish_list = open("fish.txt").readlines()
+    meat_list = open("meats.txt").readlines()
+    found_fish = []
+    replacement = {}
+	for single_ingredient in recipe['ingredients']:
+        if single_ingredient["name"] in fish_list:
+            meat_replacement = random.choice(meat_list) # TODO: we can be smarter than random
+            single_ingredient["name"] = meat_replacement
+            found_fish.append(single_ingredient["name"])
+            replacement[single_ingredient["name"]] = meat_replacement
+    for fish in found_fish:
+        for step in recipe["steps"]:
+            if fish in step["text"]:
+                step["text"] = step["text"].replace(fish, replacement[fish])
+    return recipe
 
 load_lists()
 remove_meat(test_rec)
